e7ca8d480c2fb7a004ca177e6209ab5c98693c8f
miguel angel clavijo moreno <miguelpositive@gmail.com>
2023-06-27 22:06:54 -0500

    feat (config): agrego el archivo de conexion de la base de datos

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const { connectionDb } = require("./config/connectiondb.js");
 
 const app = express();
 
 const port = process.env.PORT || 4000;
+
+dotenv.config();
+connectionDb();
 
 app.listen(port, () => {
   console.log(`servidor backend levantado en el puerto ${port}`);
