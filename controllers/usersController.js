3c64c477a30e467b1ed4f238f833be34343278c0
miguel angel clavijo moreno <miguelpositive@gmail.com>
2023-06-27 22:56:13 -0500

    fix (controller): creo la funcion actualizar usuario

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,7 @@
 const { usersModel } = require("../models/usersModel.js");
 
+const message = "ocurrio un error en el backend al intentar";
+
 const createUser = async (req, res) => {
   try {
     const { name, email, contact, user, password } = req.body;
@@ -10,9 +12,22 @@ const createUser = async (req, res) => {
 
     res.sendStatus(200);
   } catch (error) {
-    console.log(
-      `ocurrio un error en el backend al intentar crear el usuario: ${error}`
+    console.log(`${message} crear el usuario: ${error}`);
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const { name, email, contact, user, password, _id } = req.body;
+
+    await usersModel.updateOne(
+      { _id },
+      { $set: { name, email, contact, user, password } }
     );
+
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(`${message} actualizar el usuario`);
   }
 };
 
