294d675b491ef4f40a262f80ac19c8a3d084720d
miguel angel clavijo moreno <miguelpositive@gmail.com>
2023-06-27 23:00:46 -0500

    fix (route): agrego la ruta de actualizar usuario

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -31,4 +31,4 @@ const updateUser = async (req, res) => {
   }
 };
 
-module.exports = { createUser };
+module.exports = { createUser, updateUser };
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createUser } = require("../controllers/usersController.js");
+const { createUser, updateUser } = require("../controllers/usersController.js");
 
 const usersRouter = express.Router();
 
 usersRouter.post("/create-user", createUser);
+usersRouter.post("/update-user", updateUser);
 
 module.exports = { usersRouter };
