9ba156b3dae1214919b739a534282c949f9f1cc6
miguel angel clavijo moreno <miguelpositive@gmail.com>
2023-06-27 23:05:28 -0500

    fix (route): agrego la ruta para funcio eliminar usuario del controlador

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { createUser, updateUser } = require("../controllers/usersController.js");
+const {
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/usersController.js");
 
 const usersRouter = express.Router();
 
 usersRouter.post("/create-user", createUser);
 usersRouter.post("/update-user", updateUser);
+usersRouter.post("/delete-user", deleteUser);
 
 module.exports = { usersRouter };
